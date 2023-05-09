7080a0698125fa4f86de2b5327af922dd5238766
anarmh <anarmh@code.edu.az>
2023-05-09 11:07:06 +0400

    add ProductDetailVM

--- FiorelloFront/FiorelloFront/Controllers/ProductController.cs
+++ FiorelloFront/FiorelloFront/Controllers/ProductController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FiorelloFront.Data;
+using FiorelloFront.Models;
+using FiorelloFront.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FiorelloFront.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index(int? id)
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            if (id is null) return BadRequest();
+            Product product = await _context.Products.Include(m=>m.ProductImages).Include(m=>m.Category).Where(m=>!m.SoftDelete).FirstOrDefaultAsync(x => x.Id == id);
+
+            if (product is null) return NotFound();
+
+            ProductDetailVM model = new()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                ProductImages = product.ProductImages.ToList()
+            };
+           
+            return View(model);
         }
     }
 }
---
+++ FiorelloFront/FiorelloFront/ViewModels/ProductDetailVM.cs
@@ -0,0 +1,13 @@
+﻿using FiorelloFront.Models;
+
+namespace FiorelloFront.ViewModels
+{
+    public class ProductDetailVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CategoyName { get; set; }
+        public decimal Price { get; set; }
+        public List<ProductImage> ProductImages { get; set; }
+    }
+}
